c3c9d104ca4329a857aa0acb7c81b401db606758
John Gomez <zemog1916@gmail.com>
2024-09-26 20:08:45 -0500

    first commit

---
+++ composeApp/src/commonMain/kotlin/org/johngom/movieskmp/Movie.kt
@@ -0,0 +1,11 @@
+package org.johngom.movieskmp
+
+data class Movie(val id: Int, val title: String, val poster: String)
+
+val movies = (1..100).map {
+    Movie(
+        id = it,
+        title = "Movie $it",
+        poster = "https://picsum.photos/id/$it/100/100"
+    )
+}
---
+++ composeApp/src/iosMain/kotlin/org/johngom/movieskmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.johngom.movieskmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
